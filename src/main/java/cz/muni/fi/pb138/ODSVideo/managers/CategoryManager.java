c4f990f3dcccde9ac171354a2e1b2f86c6ccf238
janmichalov <xmicha20@aisa.fi.muni.cz>
2018-05-03 15:32:46 +0200

    added javaDocs to categoryManager and IOUtility

--- src/main/java/cz/muni/fi/pb138/ODSVideo/managers/CategoryManager.java
+++ src/main/java/cz/muni/fi/pb138/ODSVideo/managers/CategoryManager.java
@@ -2,16 +2,65 @@ package cz.muni.fi.pb138.ODSVideo.managers;
 
 import cz.muni.fi.pb138.ODSVideo.models.Category;
 import cz.muni.fi.pb138.ODSVideo.models.Movie;
+import cz.muni.fi.pb138.ODSVideo.exceptions.ValidationException;
+import cz.muni.fi.pb138.ODSVideo.exceptions.IllegalEntityException;
 
 import java.util.Collection;
 
 
 public interface CategoryManager {
-    void createCategory(Category category);
+    /**
+     * Adds category in parameter to Map<String,Category>
+     * @throws IllegalArgumentException when category is null
+     * @throws IllegalEntityException when category is already in Map
+     * @throws ValidationException when any of the category's attributes are null
+     * @param category category to be added
+     */
+    void createCategory(Category category) throws ValidationException, IllegalEntityException;
+
+    /**
+     * Deletes category from map, if category doesn't exist, nothing happens
+     * @throws IllegalArgumentException when name is null
+     * @param name name of category
+     */
     void deleteCategory(String name);
-    void updateCategory(Category category);
+
+    /**
+     * updates existing category
+     * @throws IllegalArgumentException when category is null
+     * @throws IllegalEntityException when category is not present map
+     * @throws ValidationException when category has null attributes
+     * @param category category to be updated
+     */
+    void updateCategory(Category category) throws IllegalEntityException, ValidationException;
+
+    /**
+     * tries to find category in map
+     * @throws IllegalArgumentException when name is null
+     * @param name name of category
+     * @return Category present in map or null if operation fails
+     */
     Category findCategory(String name);
-    void moveMovie(Category from, Category into, Movie movie);
+
+    /**
+     * moves movie from one category to the other
+     * @throws IllegalArgumentException if any of parameters are null
+     * @throws IllegalEntityException if any category is not in Map or movie is not in first category
+     * @param from category from which the movie is moved
+     * @param into category into which the movie is inserted
+     * @param movie movie to be moved
+     */
+    void moveMovie(String from, String into, Movie movie) throws IllegalEntityException;
+
+    /**
+     * returns all categories in map
+     * @return immutable collection of categories in map
+     */
     Collection<Category> findAllCategories();
+
+    /**
+     * returns all movies in all categories
+     * @return immutable collection of movies in all categories
+     */
     Collection<Movie> findAllMovies();
 }
--- src/main/java/cz/muni/fi/pb138/ODSVideo/managers/IOUtility.java
+++ src/main/java/cz/muni/fi/pb138/ODSVideo/managers/IOUtility.java
@@ -1,31 +1,43 @@
 package cz.muni.fi.pb138.ODSVideo.managers;
 
 import cz.muni.fi.pb138.ODSVideo.models.Category;
+import org.odftoolkit.simple.TextDocument;
+
+import java.io.IOException;
 import java.util.Map;
 
 /**
  *
  */
-public interface IOManager {
-
-    atribut TextDocument
+public interface IOUtility {
 
     /**
-     * Reads content of .ods file specified in contructor
-     *
+     * Reads content of .ods file specified in path
+     * @throws IOException if goes wrong with file (read, access...)
+     * @return content of file in form of TextDocument
      */
-    void readFile();
+    TextDocument readFile(String path) throws IOException;
 
     /**
-     * Writes into the .ods file
+     * Writes TextDocument into the .ods file specified in path
+     * @throws IllegalArgumentException if any of parameters are null
      */
-    void writeFile();
+    void writeFile(String path, TextDocument document);
 
     /**
-     * Transforms .ods document into map of categories and its names
-     * @return Map of categories
+     * Transforms .ods document into map of categories and its names as key
+     * @param document document to transformed
+     * @throws IllegalArgumentException if document is null
+     * @return map of categories containing data from document
      */
-    Map<String,Category> transformToMap();
+    Map<String,Category> transformToMap(TextDocument document);
 
-    TextD
+    /**
+     * Transforms map of categories to TextDocument format
+     * @param categoryMap map to be transformed
+     * @throws IllegalArgumentException if map is null
+     * @return TextDocument containg data from map
+     */
+    TextDocument transformToDocument(Map<String,Category> categoryMap);
+
 }
