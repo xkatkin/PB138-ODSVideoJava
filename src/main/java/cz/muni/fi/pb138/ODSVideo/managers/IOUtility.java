f235bcfabc88fbafaa6ce93d3d924394beb7da4e
janmichalov <xmicha20@aisa.fi.muni.cz>
2018-05-09 19:30:11 +0200

    changed type from TextDocument to SpreadsheetDocument and added QOL methods to interface of IOUtility

--- src/main/java/cz/muni/fi/pb138/ODSVideo/managers/IOUtility.java
+++ src/main/java/cz/muni/fi/pb138/ODSVideo/managers/IOUtility.java
@@ -1,8 +1,9 @@
 package cz.muni.fi.pb138.ODSVideo.managers;
 
 import cz.muni.fi.pb138.ODSVideo.models.Category;
-import org.odftoolkit.simple.TextDocument;
+import org.odftoolkit.simple.SpreadsheetDocument;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
@@ -14,15 +15,34 @@ public interface IOUtility {
     /**
      * Reads content of .ods file specified in path
      * @throws IOException if goes wrong with file (read, access...)
-     * @return content of file in form of TextDocument
+     * @throws IllegalArgumentException if any of parameters are null
+     * @return content of file in form of SpreadsheetDocument
      */
-    TextDocument readFile(String path) throws IOException;
+    SpreadsheetDocument readFile(String path) throws IOException;
+
 
     /**
-     * Writes TextDocument into the .ods file specified in path
+     * Reads content of .ods file specified in path
+     * @throws IOException if goes wrong with file (read, access...)
+     * @throws IllegalArgumentException if any of parameters are null
+     * @return content of file in form of SpreadsheetDocument
+     */
+    SpreadsheetDocument readFile(File file) throws IOException;
+
+
+    /**
+     * Writes SpreadsheetDocument into the .ods file specified in path
+     * @throws IOException if write operation fails
      * @throws IllegalArgumentException if any of parameters are null
      */
-    void writeFile(String path, TextDocument document);
+    void writeFile(String path, SpreadsheetDocument document) throws IOException;
+
+    /**
+     * Writes SpreadsheetDocument into the .ods file specified in path
+     * @throws IOException if write operation fails
+     * @throws IllegalArgumentException if any of parameters are null
+     */
+    void writeFile(File file, SpreadsheetDocument document) throws IOException;
 
     /**
      * Transforms .ods document into map of categories and its names as key
@@ -30,14 +50,14 @@ public interface IOUtility {
      * @throws IllegalArgumentException if document is null
      * @return map of categories containing data from document
      */
-    Map<String,Category> transformToMap(TextDocument document);
+    Map<String,Category> transformToMap(SpreadsheetDocument document);
 
     /**
-     * Transforms map of categories to TextDocument format
+     * Transforms map of categories to SpreadsheetDocument format
      * @param categoryMap map to be transformed
      * @throws IllegalArgumentException if map is null
-     * @return TextDocument containg data from map
+     * @return SpreadsheetDocument containg data from map
      */
-    TextDocument transformToDocument(Map<String,Category> categoryMap);
+    SpreadsheetDocument transformToDocument(Map<String,Category> categoryMap);
 
 }
